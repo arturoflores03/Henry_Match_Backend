6eb93df906926ce075cd610ba3aae053ec3789a4
Arturo Flores <arturofsx@gmail.com>
2022-06-29 13:03:09 -0300

    Update userId.js

--- src/controllers/userId.js
+++ src/controllers/userId.js
@@ -5,7 +5,7 @@ const userId = (req, res) => {
   const {id} = req.params;
 
   userSchema
-  .findOne({_id:id})
+  .findById(id)
   .then( (data) => res.json(data))
   .catch((err)=> res.json({message: err}))
 }
