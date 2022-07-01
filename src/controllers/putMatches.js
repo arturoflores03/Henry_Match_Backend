9836cd276a4446610ef246e2c7f8e651f6811f55
Giuxpro <albertogiusepp@gmail.com>
2022-07-01 02:59:09 -0300

    dsad

--- src/controllers/putMatches.js
+++ src/controllers/putMatches.js
@@ -6,7 +6,7 @@ const putMatches = (req, res) =>{
     const {dislike, likeReceived, likeGiven, matches} = req.body
 
     userSchema
-        .save({_id:id},{ $set: {dislike, likeReceived, likeGiven, matches}})
+        .insertOne({_id:id},{dislike, likeReceived, likeGiven, matches})
 
         .then(data => res.json(data))
         .catch((error) => res.json({message:error}));
