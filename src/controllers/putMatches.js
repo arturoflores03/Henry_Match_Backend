8139b092a6e6bd5e9c2483d3cdf9cdccaec7dcf2
Arturo Flores <arturofsx@gmail.com>
2022-07-01 11:43:27 -0300

    Update putMatches.js

--- src/controllers/putMatches.js
+++ src/controllers/putMatches.js
@@ -6,7 +6,7 @@ const putMatches = (req, res) =>{
     const {dislike, likeReceived, likeGiven, matches} = req.body
 
     userSchema
-        .findOneAndUpdate({_id:id},{dislike, likeReceived, likeGiven, matches})
+        .updateMany({_id:id},{$push: {dislike: dislike}}, {$push:{likeReceived: likeReceived}}, {$push:{likeGiven: likeGiven}}, {$push:{matches: matches}})
 
         .then(data => res.json(data))
         .catch((error) => res.json({message:error}));
