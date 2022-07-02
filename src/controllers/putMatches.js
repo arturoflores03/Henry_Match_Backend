3a498dfd7a7011eee0527ad2d98632d59c183ca2
Arturo Flores <arturofsx@gmail.com>
2022-07-02 02:17:20 -0300

    Update putMatches.js

--- src/controllers/putMatches.js
+++ src/controllers/putMatches.js
@@ -3,10 +3,10 @@ const userSchema = require("../models/user");
 
 const putMatches = (req, res) =>{
     const {id} = req.params;
-    const {dislike, likeReceived, likeGiven, matches} = req.body
+    const {dislike, likeReceived, likeGiven, matches, dislikeReceived} = req.body
 
     userSchema
-         .updateOne({_id:id},{ $push: {dislike, likeReceived, likeGiven, matches}})        
+         .updateOne({_id:id},{ $push: {dislike, likeReceived, likeGiven, matches, dislikeReceived}})        
         .then(data => res.json(data))
         .catch((error) => res.json({message:error}));
 }
