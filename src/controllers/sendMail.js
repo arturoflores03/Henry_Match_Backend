41b7f8b7c62b7fd00791d711e6a7a2dc3e89add1
JuanjoChiliutti <95381544+JuanjoChiliutti@users.noreply.github.com>
2022-07-11 18:53:45 -0300

    Update sendMail.js

--- src/controllers/sendMail.js
+++ src/controllers/sendMail.js
@@ -14,38 +14,39 @@ const sendMail = (req, res) => {
       pass: PASS,
     },
   });
-//   if (isActive === false) {
-    const mailOptions = {
+  const mailOptions = {};
+   if (isActive === false) {
+    mailOptions = {
       from: "Henry Match ",
       to: email,
       subject: "Cuenta inhabilitada Henry Match",
       text: `Hola ${name}, tu cuenta ha sido inhabilitada. Por favor, contacta con el administrador para más información.`,
     };
-//   }
-//   if (premium === true) {
-//     const mailOptions = {
-//       from: "Henry Match ",
-//       to: email,
-//       subject: "Cuenta premium Henry Match",
-//       text: `Hola ${name}, tu cuenta ha sido habilitada para uso premium.`,
-//     };
-//   }
-//   if (message !== "") {
-//     const mailOptions = {
-//       from: "Henry Match ",
-//       to: email,
-//       subject: "Mensaje Henry Match",
-//       text: `Hola ${name}, recibiste un nuevo mensaje`,
-//     };
-//   }
-//   if (matches.length > 0) {
-//     const mailOptions = {
-//       from: "Henry Match ",
-//       to: email,
-//       subject: "Nuevo match Henry Match",
-//       text: `Hola ${name}, recibiste un nuevo match`,
-//     };
-//   }
+  }
+  if (premium === true) {
+    mailOptions = {
+      from: "Henry Match ",
+      to: email,
+      subject: "Cuenta premium Henry Match",
+      text: `Hola ${name}, tu cuenta ha sido habilitada para uso premium.`,
+    };
+  }
+  if (message !== "") {
+    mailOptions = {
+      from: "Henry Match ",
+      to: email,
+      subject: "Mensaje Henry Match",
+      text: `Hola ${name}, recibiste un nuevo mensaje`,
+    };
+  }
+  if (matches.length > 0) {
+    mailOptions = {
+      from: "Henry Match ",
+      to: email,
+      subject: "Nuevo match Henry Match",
+      text: `Hola ${name}, recibiste un nuevo match`,
+    };
+  }
 
   transporter.sendMail(mailOptions, (error, data) => {
     if (error) {
